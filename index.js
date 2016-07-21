f595923b7a296a957654221a3a12a05d517887dc
John Beech <john.beech@bbc.co.uk>
2016-07-21 15:02:15 +0100

    Added sentience

--- index.js
+++ index.js
@@ -8,19 +8,33 @@ var bot = new SlackBot({
   id: '@gamebot'
 });
 
+var howls = ['Howwwwll~', 'Aawwwwooooh!', 'Snarl.', 'Growll', '*Pants*', 'Woof?', '{user} is a wereworlf...'];
+var howlCount = Math.floor(Math.random() * howls.length);
+
+const params = {
+    icon_url: 'https://avatars.slack-edge.com/2016-07-21/61753258595_de5830aea51e1509144e_48.jpg'
+};
+
 bot.on('start', function() {
   // more information about additional params
-  // https://api.slack.com/methods/chat.postMessage
-  const params = {
-    icon_url: 'https://avatars.slack-edge.com/2016-07-21/61753258595_de5830aea51e1509144e_48.jpg'
-  };
 
-  // define channel, where bot exist.
-  // You can adjust it there https://my.slack.com/services
-  bot.postMessageToChannel('random', 'Growwl~', params)
-    .then(finish)
-    .fail(catchError);
+  bot.on('message', function(data) {
+    // all ingoing events https://api.slack.com/rtm
+    console.log('Bot event', data);
+
+    if (data.type === 'message') {
+      if (data.text.includes('woof') || data.text.includes('wolf') || data.text.includes('wolve')) {
+        respond('random', howls[howlCount % howls.length].replace('{user}', data.user));
+        howlCount++;
+      }
+    }
+  });
 });
+
+function respond(channel, message) {
+  // https://api.slack.com/methods/chat.postMessage
+  return bot.postMessageToChannel(channel, message, params);
+}
 
 function finish(data) {
   console.log(data, JSON.stringify(data, null, 2));
