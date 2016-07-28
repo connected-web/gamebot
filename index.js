9d42f3bac1f0ecb9418295b445d576c07b322fa3
John Beech <john.beech@bbc.co.uk>
2016-07-28 10:09:06 +0100

    Reorganise main code into connect promise, with stubs

--- index.js
+++ index.js
@@ -1,48 +1,18 @@
-var SlackBot = require('slackbots');
+const api = require('./lib/api');
 
-// Create the bot
-// Needs to be set up at https://my.slack.com/services/new/bot - and set your token locally
-var bot = new SlackBot({
-  token: process.env.GAMEBOT_TOKEN, // Add a bot
-  name: 'Mr Wolf',
-  id: '@gamebot'
-});
-
-var howls = ['Howwwwll~', 'Aawwwwooooh!', 'Snarl.', 'Growll', '*Pants*', 'Woof?', '{user} is a wereworlf...'];
-var howlCount = Math.floor(Math.random() * howls.length);
-
-const params = {
-    icon_url: 'https://avatars.slack-edge.com/2016-07-21/61753258595_de5830aea51e1509144e_48.jpg'
+const options = {
+  slackbot: {
+    token: process.env.GAMEBOT_TOKEN, // Add a bot
+    name: 'Mr Wolf',
+    id: '@gamebot',
+  },
+  message: {
+    params: {
+      icon_url: 'https://avatars.slack-edge.com/2016-07-21/61753258595_de5830aea51e1509144e_48.jpg'
+    }
+  }
 };
 
-bot.on('start', function() {
-  // more information about additional params
-
-  bot.on('message', function(data) {
-    // all ingoing events https://api.slack.com/rtm
-    console.log('Bot event', data);
-
-    if (data.type === 'message') {
-      if (data.text.includes('woof') || data.text.includes('wolf') || data.text.includes('wolve')) {
-        respond('random', howls[howlCount % howls.length].replace('{user}', data.user));
-        howlCount++;
-      }
-    }
-    const users = bot.getUsers();
-    console.log('Users', JSON.stringify(users, null, 2));
-  });
-});
-
-function respond(channel, message) {
-  // https://api.slack.com/methods/chat.postMessage
-  return bot.postMessageToChannel(channel, message, params);
-}
-
-function finish(data) {
-  console.log(data, JSON.stringify(data, null, 2));
-  process.exit(0);
-}
-
-function catchError(e) {
-  console.log('Slackerr', e);
-}
+api.load()
+  .then(api.connect(options))
+  .catch(api.handleError);
