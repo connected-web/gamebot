42341a7a6660c9313c38ae4b667c9eb040e3263e
John Beech <john.beech@bbc.co.uk>
2016-12-22 14:11:57 +0000

    Create website endpoint to list users by name

---
+++ website/api/users.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+var endpoint = function () {}
+
+endpoint.route = '/api/howl/users';
+endpoint.cacheDuration = '30 seconds';
+endpoint.description = 'A list of users'
+
+endpoint.configure = function (config) {
+
+}
+
+endpoint.render = function (req, res) {
+  var data = {};
+  try {
+    var users = JSON.parse(fs.readFileSync(__dirname + '/../../state/users-list.json', 'utf8'));
+    data = users.map((user) => {
+      return name;
+    });
+  } catch (ex) {
+    data.error = true;
+    data.message = 'Unable to read data file: ' + ex;
+    data.stack = ex.stack;
+  }
+  // send response
+  res.jsonp(data);
+}
+
+module.exports = endpoint;
