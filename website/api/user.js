76f9a7b4907a2a38771bf889af69e25f741d5c72
John Beech <john.beech@bbc.co.uk>
2016-12-22 14:34:06 +0000

    Create user API endpoint

---
+++ website/api/user.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+var endpoint = function () {}
+var userIndex = false;
+
+endpoint.route = '/api/user/:name';
+endpoint.cacheDuration = '30 seconds';
+endpoint.description = 'A specific user'
+
+endpoint.configure = function (config) {
+
+}
+
+endpoint.render = function (req, res) {
+  var data = {};
+  try {
+    if (!userIndex) {
+      var users = JSON.parse(fs.readFileSync(__dirname + '/../../state/user-list.json', 'utf8'));
+      users.members.forEach((user) => {
+        userIndex[user.name] = user;
+      });
+    }
+    data = userIndex[req.params.name];
+  } catch (ex) {
+    data.error = true;
+    data.message = 'Unable to read data file: ' + ex;
+    data.stack = ex.stack;
+  }
+  // send response
+  res.jsonp(data);
+}
+
+module.exports = endpoint;
