a3cecbd336b91d3f4de0c656ad3613718ad4d202
John Beech <john.beech@bbc.co.uk>
2016-12-27 17:22:18 +0000

    Write basic test for Resistance module

---
+++ test/modules/resistanceSpec.js
@@ -0,0 +1,48 @@
+const expect = require('chai').expect;
+const api = require('../../lib/api');
+const handleMessage = require('../../lib/handleMessage');
+const resistance = require('../../lib/modules/resistance');
+const NL = '\n';
+
+describe('Resistance module', function () {
+
+  var module, gamebot;
+
+  beforeEach((done) => {
+    gamebot = api({
+      slackbot: {
+        id: '@bot'
+      }
+    });
+
+    gamebot.getUserName = (id) => {
+      return {
+        'u1': 'John',
+        'u2': 'Henrietta',
+        'u3': 'Claus'
+      }[id] || id;
+    };
+
+    gamebot.simulateMessage = (message, userId, channelId) => {
+      handleMessage(gamebot, {
+        type: 'message',
+        text: message,
+        user: userId,
+        channel: channelId
+      });
+    };
+
+    module = resistance(gamebot, true).then(done);
+  });
+
+  it('should respond to players wanting to join', (done) => {
+    gamebot.respond = (channel, response, params) => {
+      expect(response).to.equal([`Welcome to the Resistance John.`, `Your assigned code is u1.`, 'Please await further instructions from command.'].join(NL));
+      gamebot.respond = (channel, response, params) => {
+        expect(response).to.equal(`John has joined the resistance. There are now 1 players.`);
+        done();
+      }
+    };
+    gamebot.simulateMessage('join the resistance', 'u1');
+  });
+});
