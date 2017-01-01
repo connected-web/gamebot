8f5e31fa5815a64a50bf4ed9cafc3bfaed81c54a
John Beech <github@mkv25.net>
2017-01-01 23:00:01 +0000

    Allow default bot values to be overriden via config

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const tokens = require('./tokens.json');
 const options = {
   slackbot: {
     token: tokens.GAMEBOT_TOKEN, // Add a bot
-    name: 'Mr Wolf',
-    id: '@gamebot',
+    name: tokens.GAMEBOT_NAME || 'Mr Wolf',
+    id: tokens.GAMEBOT_ID || '@gamebot',
   },
   message: {
     params: {
-      icon_url: 'https://avatars.slack-edge.com/2016-07-21/61753258595_de5830aea51e1509144e_48.jpg'
+      icon_url: tokens.GAMEBOT_AVATAR || 'https://avatars.slack-edge.com/2016-07-21/61753258595_de5830aea51e1509144e_48.jpg'
     }
   }
 };
