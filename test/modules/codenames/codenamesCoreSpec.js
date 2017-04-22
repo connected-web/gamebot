e24589538d313da9f28d00ac0119266e047b5eee
John Beech <john.beech@bbc.co.uk>
2017-04-22 14:34:30 +0100

    Add help command and test to codenames module

---
+++ test/modules/codenames/codenamesCoreSpec.js
@@ -0,0 +1,32 @@
+const expect = require('chai').expect;
+const codenames = require('../../../lib/modules/codenames/codenames');
+const mockGamebot = require('../../lib/mockGamebot');
+const NL = '\n';
+
+describe('Codenames module (Core)', function () {
+
+  var module, gamebot;
+  const gameChannel = 'codenames';
+
+  beforeEach(() => {
+    gamebot = mockGamebot();
+    module = codenames(gamebot, false);
+    module.reset();
+  });
+
+  describe('Help', () => {
+    ['help', 'How do I play codenames?', 'how do I play'].forEach((command) => {
+      it('should allow a user to request help on how to play codenames', (done) => {
+        gamebot.respond = (target, response, params) => {
+          expect(target).to.equal('u1');
+          const botname = '@testbot';
+          expect(response).to.include(`You can use these commands wherever ${botname} is present; for sensitive commands please send them directly to ${botname} in private chat.`);
+          expect(response).to.include(`Provide a list of commands on how to play codenames`);
+          expect(response).to.include(`Examples: *help*`);
+          done();
+        };
+        gamebot.simulateMessage(command, 'u1');
+      });
+    })
+  });
+});
