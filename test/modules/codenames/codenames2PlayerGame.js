b9a8a0edf45b93935940feda7639714b6312b932
John Beech <john.beech@bbc.co.uk>
2017-04-29 08:38:07 +0100

    Write test for word selection after making a spy master

--- test/modules/codenames/codenames2PlayerGame.js
+++ test/modules/codenames/codenames2PlayerGame.js
@@ -26,14 +26,23 @@ describe('Codenames module (2 Player Game)', function () {
       ], done)
       gamebot.simulateMessage('start game', 'u7')
     })
+  })
 
-    it('should allow an active player to set themselves as spy master for their team', (done) => {
+  describe('Word selection', () => {
+    beforeEach(() => {
+      gamebot.simulateMessage('join game', 'u1')
+      gamebot.simulateMessage('join game', 'u7')
       gamebot.simulateMessage('start game', 'u7')
+    })
+
+    it('should pick a list of 25 words after a spy master is selected', (done) => {
       gamebot.respond = expectResponses([
         response(/^Hannah you are now a spy master$/, 'private'),
-        response(/^Hannah has been assigned as a spy master for Team Blue$/, gameChannel)
+        response(/^Hannah has been assigned as a spy master for Team Blue$/, gameChannel),
+        response(/^Locations have been identified:(\n>[A-z]+){25}$/, gameChannel)
       ], done)
-      gamebot.simulateMessage('make me spymaster', 'u7')
+
+      gamebot.simulateMessage('make me spy master', 'u7')
     })
   })
 })
