f8ed997e436cc533a7e967529eacdae5f2e14abc
John Beech <john.beech@bbc.co.uk>
2017-09-17 19:21:54 +0100

    Create build script to zip up all files and node modules

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+DESTINATION_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd "../" && pwd )"
+DIR="."
+
+rm -f $DESTINATION_DIR/gamebot.zip
+
+zip -qr9 -X $DESTINATION_DIR/gamebot.zip $DIR/emojis $DIR/lib $DIR/node_modules $DIR/records $DIR/ssh $DIR/state $DIR/test $DIR/users $DIR/website $DIR/index.js $DIR/package.js $DIR/README.md $DIR/update.sh
+
+ls -la $DESTINATION_DIR/gamebot.zip
