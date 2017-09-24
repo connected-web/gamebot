369b11d32b84351bc75ff742288970551a102ca5
John Beech <john.beech@bbc.co.uk>
2017-09-24 21:35:43 +0100

    Add love letter 2 player game spec for starting a game

---
+++ test/modules/loveletter/loveletter2PlayerGame.js
@@ -0,0 +1,31 @@
+/* global describe it beforeEach */
+const codenames = require('../../../lib/modules/loveletter/loveletter')
+const mockGamebot = require('../../lib/mockGamebot')
+const expectResponses = require('../../lib/expectResponses')
+const response = expectResponses.createResponse
+
+describe('Loveletter module (2 Player Game)', function () {
+  var module, gamebot
+  const gameChannel = 'loveletter'
+
+  beforeEach((done) => {
+    gamebot = mockGamebot()
+    codenames(gamebot, false)
+      .then((m) => {
+        module = m
+        module.reset()
+        done()
+      })
+  })
+
+  describe('Starting a game', () => {
+    it('should allow players to start a game', (done) => {
+      gamebot.simulateMessage('join game', 'u1')
+      gamebot.simulateMessage('join game', 'u2')
+      gamebot.respond = expectResponses([
+        response(/^Love Letter has begun, the player order is:\n(>[A-z]+\n){2}[A-z]+ has the first turn.$/, gameChannel)
+      ], done)
+      gamebot.simulateMessage('start game', 'u2')
+    })
+  })
+})
