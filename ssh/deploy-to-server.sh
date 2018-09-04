2b1ae60e6297f736f08251aa06f5e71745ac03ee
John Beech <johnbeech.github@mkv25.net>
2018-09-04 14:50:21 +0100

    Run build.sh to create zip file

--- ssh/deploy-to-server.sh
+++ ssh/deploy-to-server.sh
@@ -8,6 +8,7 @@ echo "Source directory contents:"
 ls -la
 
 echo "Zip the files [TODO]"
+sh ./scripts/build.sh
 
 echo "SCP the service file to the remote machine"
 scp -o "StrictHostKeyChecking no" -i $SSH_KEY_FOR_TARGET "$SCRIPT_DIR/gamebot.service" "pi@$TARGET_IP:~/" 
