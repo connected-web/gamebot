b000f24d4606c53e2afd18f8923735fdd8102c75
John Beech <johnbeech.github@mkv25.net>
2018-09-04 14:23:30 +0100

    Copy the file to home directory before moving to target directory

--- ssh/deploy-to-server.sh
+++ ssh/deploy-to-server.sh
@@ -10,7 +10,7 @@ ls -la
 echo "Zip the files [TODO]"
 
 echo "SCP the service file to the remote machine"
-scp -o "StrictHostKeyChecking no" -i $SSH_KEY_FOR_TARGET "$SCRIPT_DIR/gamebot.service" "pi@$TARGET_IP:/etc/systemd/system/" 
+scp -o "StrictHostKeyChecking no" -i $SSH_KEY_FOR_TARGET "$SCRIPT_DIR/gamebot.service" "pi@$TARGET_IP:~/" 
 
 echo "SCP the files to the remote machine: [$TARGET_IP]"
 
@@ -20,6 +20,11 @@ ssh -o "StrictHostKeyChecking no" -i $SSH_KEY_FOR_TARGET pi@$TARGET_IP <<'ENDSSH
 ls -la
 node -v
 npm -v
+
+echo "Install service file to systemd:"
+rm /etc/systemd/system/gamebot.service
+mv ~/gamebot.service /etc/systemd/system/
+
 sudo systemctl daemon-reload 
 
 systemctl enable gamebot.service
