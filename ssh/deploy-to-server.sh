d60fc00a9c26b954ef5108d611829c87ddc7b9d1
John Beech <johnbeech.github@mkv25.net>
2018-09-05 23:07:42 +0100

    Include tokens.js in build; update log statements in deploy-to-server.sh script

--- ssh/deploy-to-server.sh
+++ ssh/deploy-to-server.sh
@@ -5,42 +5,44 @@ DESTINATION="pi@$TARGET_IP"
 cd $SCRIPT_DIR
 cd ..
 
-echo "Source directory contents:"
+echo "[DtoS] Source directory contents:"
 ls -la
 
-echo "Zip the files [TODO]"
+echo "[DtoS] Zip the files:"
 npm install
 npm run create-build
 
-echo "SCP the service file to the remote machine"
+echo "[DtoS] SCP the service file to the remote machine"
 scp -o "StrictHostKeyChecking no" -i $SSH_KEY_FOR_TARGET "$SCRIPT_DIR/gamebot.service" "$DESTINATION:~/"
 
-echo "SCP the files to the remote machine: [$TARGET_IP]"
+echo "[DtoS] SCP the files to the remote machine: [$TARGET_IP]"
 scp -o "StrictHostKeyChecking no" -i $SSH_KEY_FOR_TARGET "$SCRIPT_DIR/../gamebot.zip" "$DESTINATION:~/"
 
-echo "Logging into remote machine: [$TARGET_IP]"
+echo "[DtoS] Logging into remote machine: [$TARGET_IP]"
 ssh -o "StrictHostKeyChecking no" -i $SSH_KEY_FOR_TARGET $DESTINATION <<'ENDSSH'
 
+echo "[DtoS Remote] Check files that exist on remote:"
 ls -la
+
+echo "[DtoS Remote] Check node and npm versions on remote:"
 node -v
 npm -v
 
-echo "Install service file to systemd:"
+echo "[DtoS Remote] Install service file to systemd:"
 sudo rm /etc/systemd/system/gamebot.service
 sudo mv ~/gamebot.service /etc/systemd/system/
 
-echo "Installing gamebot files to home directory:"
+echo "[DtoS Remote] Installing gamebot files to home directory:"
 unzip -qo ~/gamebot.zip -d ~/gamebot
 
-sudo systemctl daemon-reload 
+echo "[DtoS Remote] Restart gamebot service with new files"
 
+sudo systemctl daemon-reload 
 sudo systemctl enable gamebot.service
 sudo systemctl restart gamebot.service
+
+echo "[DtoS Remote] Gamebot restarted; checking status"
 sudo systemctl status gamebot.service
-
-echo "Unzip files onto target machine: [TODO]"
-
-echo "Restart gamebot service with new files: [TODO]"
 
 ENDSSH
 
