00e281847815b621401ee7fee6283479a843e365
John Beech <johnbeech.github@mkv25.net>
2019-02-05 19:22:32 +0000

    Name bots as bots

--- tokens.js
+++ tokens.js
@@ -1,7 +1,7 @@
 module.exports = [{
   'id': 'gamebot',
   'token': process.env.RESISTANCEBOT_TOKEN,
-  'name': 'Control',
+  'name': 'Resistance Bot',
   'avatar': 'https://avatars.slack-edge.com/2017-01-01/122247673123_a4dc4e9941a6cc23a234_192.png',
   'modules': [
     'accusation',
@@ -15,7 +15,7 @@ module.exports = [{
 }, {
   'id': 'spymaster',
   'token': process.env.SPYMASTERBOT_TOKEN,
-  'name': 'Spy Master',
+  'name': 'Codenames Bot',
   'avatar': 'https://ca.slack-edge.com/T3KJBUDUH-U524MQWAX-e4bd17b061a6-192',
   'modules': [
     'codenames',
@@ -26,7 +26,7 @@ module.exports = [{
 }, {
   'id': 'princessbot',
   'token': process.env.PRINCESSBOT_TOKEN,
-  'name': 'Princess Loveletter :heart:',
+  'name': 'Princess Bot',
   'avatar': 'https://ca.slack-edge.com/T3KJBUDUH-U6X8VUEG0-622d93cd3235-1024',
   'modules': [
     'loveletter',
@@ -37,7 +37,7 @@ module.exports = [{
 }, {
   'id': 'wolfbot',
   'token': process.env.WOLFBOT_TOKEN,
-  'name': 'Mr Wolf',
+  'name': 'Wolf Bot',
   'avatar': 'https://s3-us-west-2.amazonaws.com/slack-files2/avatar-temp/2017-05-30/189595889379_fcf19175428ae24e0b17.jpg',
   'modules': [
     'howl',
